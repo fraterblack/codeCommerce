3cf43b85685a4857a664ccaeccaed67037ba4264
Edvaldo da Rosa <fraterblack@bck.com.br>
2015-07-14 18:19:33 -0300

    PR

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -3,16 +3,18 @@
 namespace CodeCommerce\Http\Controllers;
 
 use Illuminate\Http\Request;
-use CodeCommerce\Category;
 use CodeCommerce\Http\Requests;
 use CodeCommerce\Http\Controllers\Controller;
+use CodeCommerce\Category;
+use CodeCommerce\Product;
 
 class StoreController extends Controller
 {
-    public function index(Category $categoryModel)
+    public function index(Category $categoryModel, Product $productModel)
     {
         $categories = $categoryModel->all();
+        $pFeatured = $productModel->featured()->get();
 
-        return view('store.index', compact('categories'));
+        return view('store.index', compact('categories', 'pFeatured'));
     }
 }
