f01de72dfbe625ad1f7914209840189fcc3f6fde
Edvaldo da Rosa <fraterblack@bck.com.br>
2015-08-21 17:57:02 -0300

    Mudança app.php

--- config/app.php
+++ config/app.php
@@ -75,12 +75,12 @@ return [
 	| This key is used by the Illuminate encrypter service and should be set
 	| to a random, 32 character string, otherwise these encrypted strings
 	| will not be safe. Please do this before deploying an application!
-	|
+	|s
 	*/
 
-	'key' => env('APP_KEY', 'SomeRandomString'),
+	'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
-	'cipher' => MCRYPT_RIJNDAEL_128,
+	'cipher' => 'AES-256-CBC',
 
 	/*
 	|--------------------------------------------------------------------------
